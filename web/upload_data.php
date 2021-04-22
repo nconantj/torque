023c0d0194809e5ba9e453b75b1423919b0ba759
nconantj <nathanial.c.jones@gmail.com>
2021-04-22 14:33:14 -0400

    Notes on implementation.

--- web/upload_data.php
+++ web/upload_data.php
@@ -11,6 +11,94 @@ if ($abrp_forward) {
 // Connect to Database
 $db = new DBAccess($db_host, $db_user, $db_pass, $db_name);
 
+/*
+ * STEPS:
+ *   1) Query user table for int_id associated with id and eml.
+ *     a) No Record: Create user, get result, and continue.
+ *     b) Record Found: Continue.
+ */
+
+/*
+"SELECT int_id FROM users WHERE eml='eml' AND  id='id'"
+IF NOT RESULT: 
+  "INSERT INTO users ( eml, id ) VALUES ('eml', 'id')"
+  IF SUCCESS: $userid = MySQLi::insert_id
+  ELSE: ERROR
+ELSE: $userid = RESULT['int_id')
+ */
+
+/*   2) Query session headers for session id based on user int_id, v, and session.
+ *     a) No Record: Create record, get result, and continue.
+ *     b) Record Found: Continue.
+ */
+ 
+/*
+"SELECT int_id FROM session_header WHERE user_id=$userid AND v='v' AND session='session'"
+IF NOT RESULT:
+  "INSERT INTO session_header (user_id, v, session) VALUES ($userid, 'v', 'session')"
+  IF SUCCESS: $sessid = MySQLi::insert_id
+  ELSE: ERROR
+ELSE: $sessid = RESULT('int_id')
+ */
+  
+/*   3) Process Data received based on the following starting substrings:
+ *     a) "default": Session Query 1 - Default Units
+ *       i) Replace "defaultUnit" with "k" for all keys.
+ *       ii) Check if session id exists in key_info_temp. NB: Just use select query and check if we have results.
+ *         a) Not Found: Insert data into key_info_temp.
+ *         b) Found: Merge data with incoming data, insert into key_info, delete all records for session.
+ */
+ 
+/*
+"SELECT key_id, long_name, short_name, default_unit, user_unit FROM key_info_temp WHERE session_id=$sessid"
+IF NOT RESULT:
+  $data = arrray();
+  FOR EACH key, value IN $_GET
+    $data_key = substitute("k" for "defaultUnit" in key)
+    $data[$data_key] = value
+  CONVERT $data TO INDEXED ARRAY WITH $sessid ENTRY
+  "INSERT INTO key_info_temp (session_id, key_id, default_unit) VALUES $data"
+  IF NOT SUCCESS: ERROR
+ELSE
+  $data = array()
+  FOR EACH row IN RESULT
+    $data[row[key_id] = array( session_id => row[session_id], long_name => row[long_name], short_name => row[short_name], user_unit => row[user_unit])
+  FOR EACH key, value IN $_GET
+    $data_key = substitute("k" for "defaultUnit" in key)
+	$data[$data_key][default_unit] = value
+  CONVERT $data TO INDEXED ARRAY
+  TRANSACTION:
+    "INSERT INTO key_info (session_id, key_id, long_name, short_name, default_unit, user_unit) VALUES $data"
+	"DELETE FROM key_info_temp WHERE session_id=$sessid"
+  IF NOT SUCCESS: ERROR
+ */
+
+
+/*     b) "profile": Session Query 2 - Vehicle Profile
+ *       i) Check if vehicle profile exists. NB Just use select query and check if we have results.
+ *         a) Not Found: Create Record, get result, and continue.
+           b) Found: Continue.
+		 ii) Link profile to session in vehicle_info
+ */
+/*     c) "user": Session Query 3 - User Info for Keys
+ *       i) Replace "userUnit," "userShortName," and "userFullName" with "k" for all keys. 
+ *         a) Transform into array ( key => array ( user_unit => <userUnit>, user_short_name => <userShortName>, user_full_name => <userFullName> ) ).
+           b) Replace any '+' except the any that are the first character with a space in all name entries. This is to account for Bolt EV cell voltages unless or until the prefix character is changed.
+ *       ii) Check if session id exists in key_info_temp.  NB: Just use select query and check if we have results.
+ *         a) Not Found: Insert data into key_info_temp.
+ *         b) Found: Merge data with incoming data, insert into key_info, delete all records for session.
+ */
+/*     d) "k": Session Query 4 - Log Data (No documentation created at this time). - Model on old code.
+ */
+ 
+// Return the response required by Torque
+echo "OK!";
+
+/*
+ * EVERYTHING BELOW WILL NO LONGER FUNCTION
+ *
+ * DELETE OR COMMENT BEFORE TESTING
+ */
 // Create an array of all the existing fields in the database
 $db_query_result = $db->get_fields($db_table) or $db->db_die();
 $dbfields[] = $db->enumerate_rows($db_query_result);
@@ -62,7 +150,5 @@ if (sizeof($_GET) > 0) {
     die ( "No URL parameters." );
 }
 
-// Return the response required by Torque
-echo "OK!";
 
 ?>
